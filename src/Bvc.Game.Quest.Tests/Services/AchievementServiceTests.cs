66ffdb00b51b9f1ed4f2a0446ca35a0934473b13
Greg Cook <greg.cook@nutrien.com>
2024-11-16 13:16:52 -0700

    - Post multiple Achievements per player - WIP

--- src/Bvc.Game.Quest.Tests/Services/AchievementServiceTests.cs
+++ src/Bvc.Game.Quest.Tests/Services/AchievementServiceTests.cs
@@ -1,5 +1,6 @@
 using Bvc.Game.Quest.Domain;
 using Bvc.Game.Quest.Services;
+using Xerris.DotNet.Core.Validations;
 
 namespace Bvc.Game.Quest.Tests.Services;
 
@@ -11,33 +12,27 @@ public class AchievementServiceTests
     {
         service = new AchievementService();
     }
-    
-    [Fact]
-    public void PostHighScoreAchievement()
-    {
-        var player = new Player { Id = 0 };
-        var gameId = 0;
-        var achievementId = 0;
-        
-        var achievement = service.PostAchievement(player, gameId, achievementId);
-
-        Assert.Equal(achievement.PlayerId, player.Id);
-        Assert.Equal(achievement.AchievementId, achievementId);
-        Assert.Equal(achievement.GameId, gameId);
-    }
 
     [Fact]
-    public void PlayerHasAchievement()
+    public void PostAchievement()
     {
         var player = new Player();
         var gameId = 0;
         var achievementId = 0;
-        
+
         var achievement = service.PostAchievement(player, gameId, achievementId);
-        
-        Assert.Equal(achievement.PlayerId, player.Id);
-        Assert.Equal(achievement.AchievementId, achievementId);
-        Assert.Equal(achievement.GameId, gameId);
-        Assert.Equal(achievement, player.Achievement);
+
+        Validate.Begin()
+            .IsNotNull(achievement, nameof(Achievement)).Check()
+            .IsEqual(player.Achievement, achievement, nameof(player.Achievement))
+            .IsEqual(achievement.PlayerId, player.Id, nameof(achievement.PlayerId))
+            .IsEqual(achievement.GameId, gameId, nameof(achievement.GameId))
+            .Check();
+    }
+
+    [Fact]
+    public void PostAchievement_MultipleForPlayer()
+    {
+        //todo
     }
 }
